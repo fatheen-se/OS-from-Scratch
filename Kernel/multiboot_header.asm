46295e1e36142af18f46a8374bc836a6e7688209
Fatheen Nizam <fatheen.se@gmail.com>
2026-07-08 09:49:05 +0530

    feat: add kernel build system and multiboot header configuration

---
+++ Kernel/multiboot_header.asm
@@ -0,0 +1,19 @@
+; multiboot_header.asm
+section .multiboot_header
+header_start:
+    ; magic number
+    dd 0xe85250d6                ; multiboot2
+    ; architecture
+    dd 0                         ; protected mode i386
+    ; header length
+    dd header_end - header_start
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ; insert optional multiboot tags here
+
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
+header_end:
